a018b75f0e944110d253873dd8c1dcba030f1195
medilies <medilies@gmail.com>
2022-10-07 21:46:54 +0100

    it works

--- src/Commands/JsonTongueCommand.php
+++ src/Commands/JsonTongueCommand.php
@@ -2,6 +2,8 @@
 
 namespace ElaborateCode\LaravelJsonTongue\Commands;
 
+use ElaborateCode\JsonTongue\Strategies\File;
+use ElaborateCode\JsonTongue\TongueFacade;
 use Illuminate\Console\Command;
 
 class JsonTongueCommand extends Command
@@ -12,6 +14,17 @@ class JsonTongueCommand extends Command
 
     public function handle(): int
     {
+        $lang_path = new File(config('json-tongue.lang-path'));
+
+        $localizaion = new TongueFacade($lang_path);
+
+        $jsons_list = $localizaion->transcribe();
+
+        foreach ($jsons_list as $json_name => $content) {
+            // TODO: Throw exception when JSON with lang code already exists
+            file_put_contents("{$lang_path}/{$json_name}.json", json_encode($content));
+        }
+
         $this->comment('All done');
 
         return self::SUCCESS;
--- tests/Feat/JsonTongueCommandTest.php
+++ tests/Feat/JsonTongueCommandTest.php
@@ -1,5 +1,16 @@
 <?php
 
+use ElaborateCode\JsonTongue\JsonFaker\JsonFaker;
+
 it('runs the command', function () {
+    $jsonFaker = JsonFaker::make(true, $this->tempTestingPath, $this->baseTestingPath)
+        ->addLocale('en', ['greetings.json' => ['Hi' => 'Hi']])
+        ->addLocale('fr', ['greetings.json' => ['Hi' => 'Salut']])
+        ->write();
+
     $this->artisan('json-tongue')->assertExitCode(0);
+
+    expect(__('Hi'))->toBe('Salut');
+
+    $jsonFaker->rollback();
 });
