d644d7ecae7b74aa9b8972ec0425597212c47b39
medilies <medilies@gmail.com>
2022-10-08 21:52:13 +0100

    Merge command outputs its phase of process

--- src/Commands/JsonTongueMergeCommand.php
+++ src/Commands/JsonTongueMergeCommand.php
@@ -20,10 +20,14 @@ class JsonTongueMergeCommand extends Command
         $lang_path = new File(config('json-tongue.lang-path', '/lang'));
 
         if ($old_jsons = $lang_path->getDirectoryJsonContent()) {
-            $this->comment('The root of lang folder is populated with JSON files.');
+            $this->warn('The root of lang folder is populated with JSON files.');
 
             if ($force || $this->confirm('Do you wish to remove JSON files that already exist in the root of the lang folder?')) {
+                $this->line('Removing old JSON files...');
+
                 $this->removeOldJsonFiles($old_jsons);
+
+                $this->line('Removed old JSON files.');
             } else {
                 $this->warn('Remove the JSON files in the root of Lang folder manually, or instruct the command to remove them!');
 
@@ -31,7 +35,14 @@ class JsonTongueMergeCommand extends Command
             }
         }
 
+        $this->line('Loading localization data...');
+
         $jsons_list = (new TongueFacade($lang_path))->transcribe();
+        // TODO: list loaded JSON files
+
+        $this->line('Localization data is loaded.');
+
+        $this->line('Creating new JSON files...');
 
         foreach ($jsons_list as $json_name => $content) {
             file_put_contents("{$lang_path}/{$json_name}.json", json_encode($content));
