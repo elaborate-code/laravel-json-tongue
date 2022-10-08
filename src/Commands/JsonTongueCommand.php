02e0058bbb1091ad432da9f4d9889e3d7f393f80
medilies <medilies@gmail.com>
2022-10-08 15:38:23 +0100

    --F|force : remove existing JSON files //exception

--- src/Commands/JsonTongueCommand.php
+++ src/Commands/JsonTongueCommand.php
@@ -4,29 +4,50 @@ namespace ElaborateCode\LaravelJsonTongue\Commands;
 
 use ElaborateCode\JsonTongue\Strategies\File;
 use ElaborateCode\JsonTongue\TongueFacade;
+use Exception;
 use Illuminate\Console\Command;
 
 class JsonTongueCommand extends Command
 {
-    public $signature = 'json-tongue';
+    public $signature = 'json-tongue
+                            {--F|force : remove existing JSON files}';
 
     public $description = 'Merge JSON files';
 
     public function handle(): int
     {
+        $force = $this->option('force');
+
         $lang_path = new File(config('json-tongue.lang-path'));
+
+        $old_jsons = $lang_path->getDirectoryJsonContent();
+
+        if ($old_jsons) {
+            if (! $force) {
+                // TODO: prompt for clearing
+                throw new Exception('The lang directory contains JSON files, make sure to clear it before running this command');
+            } else {
+                $this->removeOldJsonFiles($old_jsons);
+            }
+        }
 
         $localizaion = new TongueFacade($lang_path);
 
         $jsons_list = $localizaion->transcribe();
 
         foreach ($jsons_list as $json_name => $content) {
-            // TODO: Throw exception when JSON with lang code already exists
             file_put_contents("{$lang_path}/{$json_name}.json", json_encode($content));
         }
 
         $this->comment('All done');
 
         return self::SUCCESS;
+    }
+
+    protected function removeOldJsonFiles(array $json_list)
+    {
+        foreach ($json_list as $file_name => $abs_path) {
+            unlink($abs_path);
+        }
     }
 }
--- tests/Feat/JsonTongueCommandTest.php
+++ tests/Feat/JsonTongueCommandTest.php
@@ -1,8 +1,21 @@
 <?php
 
 use ElaborateCode\JsonTongue\JsonFaker\JsonFaker;
+use ElaborateCode\JsonTongue\Strategies\File;
 
-it('runs the command', function () {
+it('throws an exception when lang folder already has JSON files', function () {
+    $jsonFaker = JsonFaker::make(true, $this->tempTestingPath, $this->baseTestingPath)
+        ->addLocale('en', ['greetings.json' => ['Hi' => 'Hi']])
+        ->write();
+
+    file_put_contents(new File(config('json-tongue.lang-path')).'/old.json', json_encode([]));
+
+    expect(fn () => $this->artisan('json-tongue'))->toThrow(Exception::class, 'lang directory contains JSON files');
+
+    $jsonFaker->rollback();
+});
+
+it('runs the command & translates successfully', function () {
     $jsonFaker = JsonFaker::make(true, $this->tempTestingPath, $this->baseTestingPath)
         ->addLocale('en', ['greetings.json' => ['Hi' => 'Hi']])
         ->addLocale('fr', ['greetings.json' => ['Hi' => 'Salut']])
@@ -11,6 +24,16 @@ it('runs the command', function () {
     $this->artisan('json-tongue')->assertExitCode(0);
 
     expect(__('Hi'))->toBe('Salut');
+
+    $jsonFaker->rollback();
+});
+
+it('does not throw an exception and removes old JSON files when -F', function () {
+    $jsonFaker = new JsonFaker(true, $this->tempTestingPath, $this->baseTestingPath);
+
+    file_put_contents(new File(config('json-tongue.lang-path')).'/old.json', json_encode([]));
+
+    $this->artisan('json-tongue -F')->assertExitCode(0);
 
     $jsonFaker->rollback();
 });
